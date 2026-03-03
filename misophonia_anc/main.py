af7c8f16500e71eb27ef3269040c0d739b54e79b
LukasFT <github@lftm.org>
2026-03-03 12:05:59 +0000

    Refactor config handling

--- misophonia_anc/main.py
+++ misophonia_anc/main.py
@@ -10,7 +10,7 @@ from misophonia_dataset.interface import SplitT, get_data_dir
 from misophonia_dataset.main import get_dataset_from_name, get_default_datasets_names
 from misophonia_dataset.misophonia_dataset import GeneratedMisophoniaDataset, PremadeMisophoniaDataset
 
-from ._utils import load_config, preprocess_to_webdataset_pt
+from ._utils import MisophoniaANCConfig, get_shards_dir, preprocess_to_webdataset_pt
 from .model import MisophoniaANCNet
 from .train import train_model
 
@@ -21,22 +21,9 @@ app = typer.Typer(help="Misophonia ANC model training and evaluation CLI.")
 
 @app.command()
 def preprocess(
-    # General
-    split_name: Annotated[SplitT, typer.Option(..., help="Dataset split to generate (e.g., 'train', 'val', 'test')")],
-    data_in_dir: Annotated[
-        Path, typer.Option(..., help="Base directory to load audio if using Premade dataset")
-    ] = None,
-    save_dir: Annotated[
-        Path, typer.Option(..., help="Directory to save preprocessed audio. Use data_in_dir if None")
-    ] = None,
-    num_workers: Annotated[
-        int, typer.Option(..., help="Number of workers for parallel processing. Number of CPU cores if not given.")
-    ] = None,
-    name: Annotated[str, typer.Option(..., help="Name of the dataset to preprocess.")] = "demo-v1",
+    config_file: Annotated[str, typer.Argument(..., help="path to config file with dataset parameters.")],
+    split: Annotated[SplitT, typer.Option(..., help="Dataset split to generate (e.g., 'train', 'val', 'test')")],
     overwrite: Annotated[bool, typer.Option(..., help="Whether to overwrite existing preprocessed shards.")] = False,  # noqa: FBT002
-    # Generated
-    source_data: Annotated[list[str], typer.Option(..., help="Name of source datasets to mix.")] = None,
-    num_samples: Annotated[int, typer.Option(..., help="Number of samples to generate")] = 10,
 ) -> None:
     """
     Pre-process data so it is as expected for the Misophonia ANC model.
@@ -44,74 +31,73 @@ def preprocess(
     Will create a WebDataset with .tar shards containing the preprocessed data.
     It will contain torch files that are efficient to load during training and evaluation.
     """
-    data_in_dir = data_in_dir or get_data_dir()
+    config = MisophoniaANCConfig.from_yaml(config_file)
+    split_config = config.dataset_splits[split]
+    shards_dir = get_shards_dir(config, split)
 
-    premade_dir = get_data_dir(dataset_name=name, base_dir=data_in_dir)
-    if premade_dir.exists():
-        print(f"Using premade dataset from {data_in_dir} with name {name} and split {split_name}")
-        misophonia_dataset = PremadeMisophoniaDataset(name=name, base_save_dir=data_in_dir)
-        split = misophonia_dataset.get_split(split_name)
+    if shards_dir.exists():
+        if overwrite:
+            print(f"Overwriting existing preprocessed dataset for split {split} with config: {split_config}")
+            print(f"Deleting existing shards in {shards_dir}...")
+            for file in shards_dir.glob("*"):
+                file.unlink()
+            print("Existing shards deleted.")
+        else:
+            print(f"Using existing preprocessed dataset for split {split} with config: {split_config}")
+            print("Use --overwrite to overwrite existing preprocessed dataset.")
+            return
+
+    if split_config.from_premade:
+        dataset = PremadeMisophoniaDataset(name=config.dataset_name, base_save_dir=config.dataset_base_save_dir)
+        dataset_split = dataset.get_split(split)
     else:
-        dataset_names = get_default_datasets_names() if source_data is None or len(source_data) == 0 else source_data
-        datasets = tuple(get_dataset_from_name(name, base_dir=data_in_dir) for name in dataset_names)
-        print("Generating dataset using source datasets:", datasets)
-        misophonia_dataset = GeneratedMisophoniaDataset(source_data=datasets)
-        split = misophonia_dataset.get_split(split_name, num_samples=num_samples)
+        assert split_config.generated_source_data is not None, "source_data must be provided if from_premade is False"
+        assert split_config.generated_config is not None, "generated_config must be provided if from_premade is False"
 
-    save_dir = Path(save_dir or data_in_dir)
-    out_dir = save_dir / name / split_name
-    try:
-        dataset_glob = preprocess_to_webdataset_pt(
-            out_dir,
-            split,
-            num_workers=num_workers,
-            overwrite=overwrite,
+        source_data = tuple(
+            get_dataset_from_name(name, base_dir=config.dataset_base_save_dir)
+            for name in split_config.generated_source_data
         )
-    except FileExistsError:
-        print("File exists and --overwrite not set. Skipping preprocessing.")
-        return
+        dataset = GeneratedMisophoniaDataset(source_data=source_data)
+        dataset_split = dataset.get_split(split, **split_config.generated_config)
 
-    print("Dataset glob: ", dataset_glob)
+    dataset_glob = preprocess_to_webdataset_pt(
+        shards_dir,
+        dataset_split,
+        num_workers=config.num_workers,
+    )
+    print(f"Saved preprocessed data to: {dataset_glob}")
 
 
 @app.command()
-def train(config: Annotated[str, typer.Option(..., help="path to config file with training parameters.")]) -> None:
+def train(
+    config_file: Annotated[str, typer.Argument(..., help="path to config file with training parameters.")],
+) -> None:
     """
     Train the Misophonia ANC model.
     """  # TODO: Improve docs
-    if config is None:
-        raise ValueError("Please specify config file path for training.")
+    config = MisophoniaANCConfig.from_yaml(config_file)
 
-    config = load_config(config)
-
-    shard_path = config.get("shard_path")
-    num_epochs = config.get("train_params", {}).get("num_epochs", 10)
-    batch_size = config.get("train_params", {}).get("batch_size", 1)
-    num_workers = config.get("train_params", {}).get("num_workers", 0)
-    model_params = config.get("model_params", {})
-
-    if shard_path is None:
-        raise ValueError("Path to webdataset shards must be provided for training.")
-    shard_glob = glob.glob(shard_path)
+    train_glob = glob.glob(str(get_shards_dir(config, "train") / "data-*.tar"))
     train_data = (
-        wds.WebDataset(shard_glob)
+        wds.WebDataset(train_glob)
         .shuffle(1000)  # optional
         .decode("torch")  # converts the saved numpy arrays to tensors
         .to_tuple("mix.npy", "gt.npy", "label.npy")
-        .batched(batch_size)
+        .batched(config.batch_size)
     )
 
-    train_loader = wds.WebLoader(train_data, batch_size=None, num_workers=num_workers)
+    train_loader = wds.WebLoader(train_data, batch_size=None, num_workers=config.num_workers)
 
-    model = MisophoniaANCNet(**model_params)  # noqa: F841
+    model = MisophoniaANCNet(**config.model_params)  # noqa: F841
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     train_model(
         model,
         device=device,
         train_loader=train_loader,
-        batch_size=batch_size,
-        n_epochs=num_epochs,
-        num_workers=num_workers,
+        batch_size=config.batch_size,
+        n_epochs=config.num_epochs,
+        num_workers=config.num_workers,
         log_dir=None,
     )
 
