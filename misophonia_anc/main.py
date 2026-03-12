f988ce798ddfde649f0e347a60c05c9cc769e4f1
Lukas Frimer Tholander <github@lftm.org>
2026-03-12 13:56:45 +0000

    Improve logging

--- misophonia_anc/main.py
+++ misophonia_anc/main.py
@@ -129,18 +129,17 @@ def train(
         dataset_dir = Path(fast_data_dir) / name / "webdataset"
         dataset_dir.mkdir(parents=True, exist_ok=True)
         eliot.log_message(
-            f"Copying preprocessed data from {dataset_dir_orig} to {dataset_dir} using rsync...", level="info"
+            f"Copying preprocessed data from {dataset_dir_orig} to {dataset_dir} using rsync... May take a while for nodes that have not yet loaded the data into the fast data dir.",
+            level="info",
         )
-        print(
-            "May take a while for nodes that have not yet loaded the data into the fast data dir."
-        )  # TODO: Change print statments to logging
         subprocess.run(["rsync", "-a", "--delete", str(dataset_dir_orig) + "/", str(dataset_dir) + "/"], check=True)
         eliot.log_message(f"Copied preprocessed data to {dataset_dir}.", level="debug")
 
     shards_glob_train = glob.glob(str(dataset_dir / "train" / "data-*.tar"))
-    print(f"Loading data from `{shards_glob_train[0]}` etc...")
-    print(
-        f"Using {num_workers} workers loading WebDataset (total CPU count = {os.cpu_count()}, allocated = {get_allocated_cpus()})."
+    eliot.log_message(f"Loading data from `{shards_glob_train[0]}` etc...", level="debug")
+    eliot.log_message(
+        f"Using {num_workers} workers loading WebDataset (total CPU count = {os.cpu_count()}, allocated = {get_allocated_cpus()}).",
+        level="debug",
     )
 
     train_data = (
@@ -166,6 +165,7 @@ def train(
 
     model = MisophoniaANCNet(**config.model_params)  # noqa: F841
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    eliot.log_message(f"Using device: {device}", level="debug")
     train_model(
         model,
         device=device,
