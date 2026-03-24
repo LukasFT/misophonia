fixed masking

misophonia_anc/train.py
@@ -37,6 +37,7 @@ from torchmetrics.functional import (
 )
 
 from ._utils import print_mem
+
 # from .model import MisophoniaANCNet
 
 
@@ -56,22 +57,22 @@ def train_epoch(
 
     batch_train_losses = []
 
-    for batch_idx, (inputs, gt, pad_lens) in enumerate(train_loader):
+    for batch_idx, (inputs, gt, audio_lens) in enumerate(train_loader):
         # in loader return mask that is [B, C, N]
         inputs = {k: v.to(device) for k, v in inputs.items()}
         gt = gt.to(device)
-        pad_lens = pad_lens.to(device)
-        B = gt.shape[0]
+        audio_lens = audio_lens.to(device)
+        _, _, T = gt.shape
 
         # print_mem("after inputs")
         optimizer.zero_grad()
 
+        # Mask output
         output = model(inputs)
-        # ugly but memory efficient
-        for b in range(B):
-            end = pad_lens[b].item()  # scalar
-            output["x"][b, :, end:] = 0
-        # print_mem("after forward")
+        pred = output["x"]
+        time_idx = torch.arange(T, device=pred.device).unsqueeze(0)
+        mask = (time_idx < audio_lens.unsqueeze(1)).unsqueeze(1)
+        output["x"] = pred * mask
 
         loss = loss_fn(output, gt)
         loss.backward()
@@ -105,18 +106,19 @@ def val_epoch(
     val_si_snrs = []
 
     with torch.no_grad():
-        for batch_idx, (inputs, gt, pad_lens) in enumerate(val_loader):
+        for batch_idx, (inputs, gt, audio_lens) in enumerate(val_loader):
             inputs = {k: v.to(device) for k, v in inputs.items()}  # [B, 2, N]
-            B = gt.shape[0]
+            _, _, T = gt.shape
 
             gt = gt.to(device)
-            pad_lens = pad_lens.to(device)
+            audio_lens = audio_lens.to(device)
 
+            # Mask output
             output = model(inputs)
-            # ugly but memory efficient
-            for b in range(B):
-                end = pad_lens[b].item()  # scalar
-                output["x"][b, :, end:] = 0
+            pred = output["x"]
+            time_idx = torch.arange(T, device=pred.device).unsqueeze(0)
+            mask = (time_idx < audio_lens.unsqueeze(1)).unsqueeze(1)
+            output["x"] = pred * mask
 
             loss = loss_fn(output, gt)
 