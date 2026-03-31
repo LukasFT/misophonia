67e1a7238ad32269c0cb5040d01676b772f8453e
Tonio Ermakoff <termakoff@gmail.com>
2026-03-31 11:38:44 +0000

    fixed mixing bug

--- misophonia_dataset/mixing.py
+++ misophonia_dataset/mixing.py
@@ -147,16 +147,10 @@ def _normalize_and_pad(
         for (item, audio), rms in zip(bg_tracks, rms_bg)
     )
 
-    fg_max_end = max(track.end for track, _ in fg_norm)
+    fg_max_end = max(audio.shape[-1] for _, audio in fg_norm)
     # Pad in case that the audio is shorter than max fg audio
     fg_padded = tuple((track, np.pad(audio, (track.start, fg_max_end - track.end))) for track, audio in fg_norm)
     bg_padded = tuple((track, np.pad(audio, (track.start, fg_max_end - track.end))) for track, audio in bg_norm)
 
-<<<<<<< HEAD
-    print("Max length:", fg_max_end)
-    print("FG lengths:", [audio.shape for _, audio in fg_padded + bg_padded])
-
-=======
->>>>>>> 7f523b5 (resolved consistency issues in variable naming and roles)
-    assert all(len(audio) == fg_max_end for _, audio in fg_padded + bg_padded)
+    assert all(audio.shape[-1] == fg_max_end for _, audio in fg_padded + bg_padded)
     return fg_padded, bg_padded
