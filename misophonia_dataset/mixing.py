4b39d6268d08457f3fc5911bb824b688d95e5a92
Tonio Ermakoff <termakoff@gmail.com>
2026-03-31 11:58:09 +0000

    audio padding debuggin

--- misophonia_dataset/mixing.py
+++ misophonia_dataset/mixing.py
@@ -147,7 +147,7 @@ def _normalize_and_pad(
         for (item, audio), rms in zip(bg_tracks, rms_bg)
     )
 
-    fg_max_end = max(audio.shape[-1] for _, audio in fg_norm)
+    fg_max_end = max(track.end for track, _ in fg_norm)
     # Pad in case that the audio is shorter than max fg audio
     fg_padded = tuple((track, np.pad(audio, (track.start, fg_max_end - track.end))) for track, audio in fg_norm)
     bg_padded = tuple((track, np.pad(audio, (track.start, fg_max_end - track.end))) for track, audio in bg_norm)
