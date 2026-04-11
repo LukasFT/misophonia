5f26ed79e36b6f0ffe7a3045bf521caf36c28860
Tonio Ermakoff <termakoff@gmail.com>
2026-04-11 04:21:28 -0500

    small typo

--- misophonia_anc/_utils.py
+++ misophonia_anc/_utils.py
@@ -367,6 +367,7 @@ def perform_inference(
     with torch.no_grad():
         for idx, (inp, gt, audio_len) in enumerate(data_loader):
             # inputs = {k: v.to(device) for k, v in inp.items()}
+            inputs = {}
             inputs["mix"] = inp["mix"].to(device)
             inputs["label_vector"] = inp["label_vector"].to(device)
             inputs["is_control"] = inp["is_control"].to(device)
