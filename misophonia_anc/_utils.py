4e2448324c98c2851588baf473037ec3d1570aa1
Tonio Ermakoff <termakoff@gmail.com>
2026-04-11 04:14:24 -0500

    refactor just for debugging

--- misophonia_anc/_utils.py
+++ misophonia_anc/_utils.py
@@ -366,7 +366,10 @@ def perform_inference(
     model.eval()
     with torch.no_grad():
         for idx, (inp, gt, audio_len) in enumerate(data_loader):
-            inputs = {k: v.to(device) for k, v in inp.items()}
+            # inputs = {k: v.to(device) for k, v in inp.items()}
+            inputs["mix"] = inputs["mix"].to(device)
+            inputs["label_vector"] = inputs["label_vector"].to(device)
+            inputs["is_control"] = inputs["is_control"].to(device)
             gt = gt.to(device)
 
             output = model(inputs)
