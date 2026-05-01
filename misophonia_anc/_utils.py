b881575ce114b510e57cbe345a41f363d3eab4a1
LukasFT <github@lftm.org>
2026-05-01 00:55:22 +0000

    Fix logging

--- misophonia_anc/_utils.py
+++ misophonia_anc/_utils.py
@@ -1055,7 +1055,7 @@ def _debug_to_mlflow(
                     torch.cuda.memory_reserved(device) - torch.cuda.memory_allocated(device)
                 )
                 / (1024**3),
-                f"debug/{prefix}_batch_vram_total_gb": (
+                f"debug/{prefix}batch_vram_total_gb": (
                     torch.cuda.get_device_properties(device).total_memory / (1024**3)
                 ),
             },
